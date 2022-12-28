9e45939d27780551068015ec9edeb4902723dc54
Luciano Coronel <86379664+lucho9595@users.noreply.github.com>
2022-12-28 16:50:39 -0300

    fix

--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -9,34 +9,34 @@ import { persona } from '../model/persona.model';
 })
 export class PersonaService {
   URL = environment.URL + 'personas/';
-  constructor(private htppClient: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   //se utiliza el Observable para las busquedas asincronas
 //traemos a toda la lista de experiencia
 public lista(): Observable<persona[]> {
-  return this.htppClient.get<persona[]>(this.URL + 'lista');
+  return this.http.get<persona[]>(this.URL + 'lista');
 }
 
 //buscamos la experiencia por el id
 public detail(id: number): Observable<persona>{
-  return this.htppClient.get<persona>(this.URL + `detail/${id}`)
+  return this.http.get<persona>(this.URL + `detail/${id}`)
 }
 
 
 //aca actualizamos los datos
 public update(id: number, Persona: persona): Observable<any>{
-  return this.htppClient.put<any>(this.URL + `update/${id}`, Persona)
+  return this.http.put<any>(this.URL + `update/${id}`, Persona)
 }
 
 /*//creamos una nuevo experiencia
 public save(education: Education): Observable<any>{
-  return this.htppClient.post<any>(this.URL + `create`, education);
+  return this.http.post<any>(this.URL + `create`, education);
 }
 
 
 //aca borramos la experiencia
 public delete(id: number): Observable<any>{
-  return this.htppClient.delete<any>(this.URL + `delete/${id}`)
+  return this.http.delete<any>(this.URL + `delete/${id}`)
 }*/
 
 }
