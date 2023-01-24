dd734b8873fc13b3b6e55ce54e3021aef546d97a
Luciano Coronel <86379664+lucho9595@users.noreply.github.com>
2023-01-24 16:55:36 -0300

    add fecha

--- src/app/model/education.ts
+++ src/app/model/education.ts
@@ -3,10 +3,14 @@ export class Education {
     nombreE :  string;
     descripcionE : string;
     tituloE: string;
- 
-    constructor(nombreE:string, descripcionE: string, tituloE: string){
+    inicioE: Date;
+    finE: Date;
+
+    constructor(nombreE:string, descripcionE: string, tituloE: string, inicioE: Date, finE: Date){
         this.nombreE = nombreE;
         this.descripcionE = descripcionE;
         this.tituloE = tituloE;
+        this.inicioE = inicioE;
+        this.finE= finE;
     }
 }
--- src/app/model/experiencie.ts
+++ src/app/model/experiencie.ts
@@ -2,9 +2,13 @@ export class Experiencie {
     id? : number;
     nombreE :  string;
     descripcionE : string;
- 
-    constructor(nombreE:string, descripcionE: string){
+    inicioE: Date;
+    finE: Date;
+
+    constructor(nombreE:string, descripcionE: string, inicioE: Date, finE: Date){
         this.nombreE = nombreE;
         this.descripcionE = descripcionE;
+        this.inicioE = inicioE;
+        this.finE= finE;
     }
 }
